33a07743923be5dc545e416fc56c7ce656727ec8
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-04-26 14:56:24 -0600

    Created User model and add user when auth fires

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 require('dotenv').config()
+const User = require('../models/user');
 
 passport.use(
   new GoogleStrategy({
@@ -11,5 +12,12 @@ passport.use(
     //passport callback function
     console.log(profile);
     console.log("Passport callback function fired!")
+    new User({
+      username: profile.emails[0].value,
+      userID: profile.id
+    }).save()
+    .then((newUser) => {
+      console.log('new user created' + newUser);
+    })
   })
 )
