76f1e3d0812d911d2a9cd8a5c63aa72aee5ec998
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-05-04 11:29:24 -0600

    change to server.js and API.js

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ require('dotenv').config();
 const passport = require('passport');
 var cors = require('cors');
 
-// const path = require("path");
+const bodyParser = require('body-parser');
+const path = require("path");
+const crypto = require('crypto');
+const multer = require('multer');
+const methodOverride = require('method-override');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,22 +22,59 @@ app.use(cors());
 
 
 // Define middleware here
+app.use(bodyParser.json());
+app.use(methodOverride('_method')); //to be used for deleting gifts
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //Passport Middleware
 app.use(passport.initialize());
-//Passport Config
-require("./config/passport")(passport);
+app.use(passport.session());
 
 // Define API routes here
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/forgetmenot", { useNewUrlParser: true });
+const LOCAL_URI = "mongodb://localhost/forgetmenot"; 
+
+// Connect to Mongo 
+// const conn = mongoose.createConnection(process.env.MONGODB_URI || "mongodb://localhost/forgetmenot", { useNewUrlParser: true });
+const conn = mongoose.createConnection(LOCAL_URI);
+
+// Initialize gridFs
+let gfs;
+
+conn.once('open', () => {
+  console.log('Connection Open');
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: LOCAL_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => { 
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads' //needs to match promise name on line 53
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+
+const upload = multer({ storage });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
